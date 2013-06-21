bcc8c28f886acb3f896621b0c23df114b877e275
Ian Macalinao <ianmacalinao@gmail.com>
2013-06-21 13:47:57 -0500

    Improve PvP Stats command
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/pvpranker/command/PvPStatsCommand.java
+++ src/main/java/net/new_liberty/pvpranker/command/PvPStatsCommand.java
@@ -1,6 +1,9 @@
 package net.new_liberty.pvpranker.command;
 
 import net.new_liberty.pvpranker.PvPRanker;
+import net.new_liberty.pvpranker.PvPer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,8 +19,33 @@ public class PvPStatsCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("Your score is " + plugin.getPvPer(sender.getName()).getScore());
+    public boolean onCommand(final CommandSender sender, Command command, String label, String[] args) {
+        String nameStr = sender.getName();
+        if (args.length > 0) {
+            nameStr = args[0];
+        }
+
+        final String name = nameStr;
+
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                PvPer pvper = plugin.getPvPer(name);
+
+                sender.sendMessage(ChatColor.YELLOW + "=== " + name + "'s PvP Stats ===");
+
+                if (pvper.getKillCount(plugin.getMilestone()) == 1) {
+                    sender.sendMessage(ChatColor.GREEN + name + " has no PvP history since the last milestone.");
+                    return;
+                }
+
+                int score = pvper.getScore(plugin.getMilestone());
+                sender.sendMessage(ChatColor.GREEN + "Rank: " + plugin.getRank(score).getName());
+                sender.sendMessage(ChatColor.GREEN + "Score: " + score);
+                return;
+            }
+        });
+
         return true;
     }
 }
